b87adb9106b39d669868efed0e133b36953491ff
Alex Koukoulas <alexk2410@hotmail.com>
2015-10-08 22:31:55 +0100

    Created pages for all the projects along with a base project template

--- mywebsite_project/webapp/urls.py
+++ mywebsite_project/webapp/urls.py
@@ -2,4 +2,10 @@ from django.conf.urls import patterns, url
 from webapp import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'))
+    url(r'^$', views.index, name='index'),
+    url(r'^pkmnrevo/$', views.pkmnrevo, name='pkmnrevo'),
+    url(r'^arkanoid/$', views.arkanoid, name='arkanoid'),
+    url(r'scrabble/$', views.scrabble, name='scrabble'),
+    url(r'tichu/$', views.tichu, name='tichu'),
+    url(r'pokyellow/$', views.pokyellow, name='pokyellow')
+    )
--- mywebsite_project/webapp/views.py
+++ mywebsite_project/webapp/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'webapp/index.html', {});
+
+def pkmnrevo(request):
+	return render(request, 'webapp/pkmnrevo.html', {});
+
+def arkanoid(request):
+	return render(request, 'webapp/arkanoid.html', {});
+
+def scrabble(request):
+	return render(request, 'webapp/scrabble.html', {});
+
+def tichu(request):
+	return render(request, 'webapp/tichu.html', {});
+
+def pokyellow(request):
+	return render(request, 'webapp/pokyellow.html', {});
